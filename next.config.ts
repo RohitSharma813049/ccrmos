7796b97cbbd6586cf14abc95bf33447ca139f37b
Rohit Sharma <rohitsharma813049@gmail.com>
2026-08-29 09:18:37 +0530

    refactor: migrate image domains to remotePatterns and add *.r2.dev wildcard support

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      "lh3.googleusercontent.com", 
-      "avatars.githubusercontent.com", 
-      "res.cloudinary.com",
-      "s3.amazonaws.com"
+    remotePatterns: [
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "s3.amazonaws.com" },
+      { protocol: "https", hostname: "*.r2.dev" },
     ],
   },
   serverExternalPackages: [
